3fa39824d4d8130380520330fe9f31f2cb1c1082
buqulignhun <2238587584@qq.com>
2017-04-15 20:27:12 +0800

    第一次提交

---
+++ app/src/main/java/com/example/dragablemenu/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.dragablemenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+        setContentView(R.layout.activity_main);
+    }
+}
